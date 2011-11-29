8b1b7974620c99499218bf23240ab16bfe466f95
Peter Lohse <peter@localhost>
2011-11-29 07:18:49 +0100

    NULL char pointer bug fixxed

--- src/general.cpp
+++ src/general.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 vector<string> general::split(string str, string delim) {
     vector<string> splitted;
-    char* part;
+    char* part = new char[400];
     
     part = strtok((char*)str.c_str(), delim.c_str());
     while(part != NULL) {
