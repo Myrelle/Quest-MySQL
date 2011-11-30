72ab5fa248e88f6173b74c2d26f54f772b6be8be
Peter Lohse <peter@localhost>
2011-11-30 15:14:51 +0100

    segmentation fault error fixxed

--- main.cpp
+++ main.cpp
@@ -47,9 +47,9 @@ int main(int argc, char** argv) {
 						gen->write_to_log("main.cpp", "False Query-Type", logfile);
 					printf("return {\"ERROR\", \"False Query-Type\"}\n");
 				}
+				mysql_close(&conn);
 			}
 			
-			mysql_close(&conn);
 			delete cnf;
 			fclose(logfile);
 		}
